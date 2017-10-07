7c64606e3f0ed5d0d824ee941d2d5a88484a6460
hardcorebadger <trefethen@Jakes-MacBook-Pro-2.local>
2017-10-07 13:28:28 -0700

    scroll preview

--- workers/unity/Assets/Gamelogic/UI/UIPreviewWindow.cs
+++ workers/unity/Assets/Gamelogic/UI/UIPreviewWindow.cs
@@ -8,15 +8,10 @@ namespace Assets.Gamelogic.Core {
 
 	public class UIPreviewWindow : MonoBehaviour {
 
-		public GameObject dotContainer;
 		public GameObject widgetContainer;
-		public GameObject dotPrefab;
-		public Color activeDot;
-		public Color inactiveDot;
 		public Text title;
 
 		private GameObject target;
-		private Dictionary<GameObject,UIPreviewWidget> dots;
 
 		public void Load(GameObject g) {
 			target = g;
@@ -25,34 +20,18 @@ namespace Assets.Gamelogic.Core {
 				if (UIManager.instance.previewWidgetsOptions.ContainsKey (m.GetType()))
 					AddWidget (UIManager.instance.previewWidgetsOptions [m.GetType()]);
 			}
-
-			// shit-tarded way of getting the first value
-			foreach (GameObject d in dots.Keys) {
-				OpenWidget (d);
-				break;
-			}
+			StartCoroutine (Redraw());
 		}
 
-		private void OpenWidget(GameObject dot) {
-			foreach (GameObject d in dots.Keys) {
-				d.GetComponent<Image> ().color = inactiveDot;
-			}
-			dot.GetComponent<Image> ().color = activeDot;
-			foreach (Transform t in widgetContainer.transform) {
-				Destroy (t.gameObject);
-			}
-			Instantiate (dots [dot], widgetContainer.transform).GetComponent<UIPreviewWidget> ().Load (this, target);
+		// I know you're thinking - woah - wtf is this about - well turns out responsive 
+		// design is easier in HTML than in Unity - so just forget about it.
+		private IEnumerator Redraw() {
+			yield return null;
+			LayoutRebuilder.MarkLayoutForRebuild (widgetContainer.transform as RectTransform);
 		}
 
 		private void AddWidget(UIPreviewWidget w) {
-			// create a dot which when clicked opens the widget
-			GameObject dot = Instantiate(dotPrefab,dotContainer.transform);
-			dot.GetComponent<Button> ().onClick.AddListener (delegate{OpenWidget (dot);});
-			dots.Add (dot,w);
-		}
-
-		private void OnEnable() {
-			dots = new Dictionary<GameObject,UIPreviewWidget> ();
+			Instantiate (w, widgetContainer.transform).GetComponent<UIPreviewWidget> ().Load (this, target);
 		}
 
 		private void Update() {
