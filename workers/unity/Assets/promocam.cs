8b256406b985d334b246a876d91b253dfdf28ce8
hardcorebadger <trefethen@Jakes-MacBook-Pro-2.local>
2017-10-10 16:01:11 -0700

    random stuff

---
+++ workers/unity/Assets/promocam.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class promocam : MonoBehaviour {
+
+	public float speed = 5f;
+	public float xmult = 1f;
+	public float zmult = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float f = Time.deltaTime * speed;
+		transform.position += new Vector3 (f*xmult, 0, f*zmult);
+	}
+}
---
+++ workers/unity/Assets/promoguy.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class promoguy : MonoBehaviour {
+
+	public Vector3 dir;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += Time.deltaTime * speed * dir;
+	}
+}
